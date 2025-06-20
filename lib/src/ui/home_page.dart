c051c5c4de45311468c088379e7230497aa71f19
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-20 11:25:35 -0300

    Home
    Popup Menu

--- lib/src/ui/home_page.dart
+++ lib/src/ui/home_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:not_whatsapp/src/ui/home/chats_tab.dart';
 import 'package:not_whatsapp/src/ui/home/contacts_tab.dart';
+import 'package:not_whatsapp/src/ui/login_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,13 +11,35 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   final _auth = FirebaseAuth.instance;
 
   late final TabController _tabController;
 
+  final List<String> _menuItems = ["Settings", "Logout"];
+
   _getUserData() {
     var user = _auth.currentUser;
+  }
+
+  _menuChoice(String menu) {
+    switch (menu) {
+      case "Settings":
+        print("Settings");
+        break;
+      case "Logout":
+        _logout();
+        break;
+    }
+  }
+
+  _logout() async {
+    await _auth.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
   }
 
   @override
@@ -32,6 +55,16 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     return Scaffold(
       appBar: AppBar(
         title: Text("Not Whatsapp"),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: _menuChoice,
+            itemBuilder: (context) {
+              return _menuItems.map((menuItem) {
+                return PopupMenuItem(value: menuItem, child: Text(menuItem));
+              }).toList();
+            },
+          ),
+        ],
         bottom: TabBar(
           indicatorWeight: 4.0,
           controller: _tabController,
