0441f1424fe4caeb7f3920310643793b9bfb2d1c
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-20 01:02:46 -0300

    Home
    Retrieve user data

--- lib/src/ui/home_page.dart
+++ lib/src/ui/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,13 +9,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _auth = FirebaseAuth.instance;
+
+  var _userEmail = "";
+
+  _getUserData() {
+    var user = _auth.currentUser;
+
+    setState(() {
+      _userEmail = user?.email ?? "";
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserData();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Not Whatsapp"),
       ),
-      body: Container(),
+      body: Container(
+        child: Text(_userEmail),
+      ),
     );
   }
 }
